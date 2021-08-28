00818cd5d7d6879587321f039ad1b963b3b297a6
Michael Sanford <michaelsanford@users.noreply.github.com>
2021-08-27 22:05:22 -0400

    Add Kaggle, inline language cursor

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const available = [...document.querySelectorAll("section")].map(e => e.getAttribute("lang"));
-const nodes = [...document.querySelectorAll("section")];
-
-let cursor = 0;
-setInterval(() => {
-    nodes[cursor].classList.remove("show");
-    cursor = ++cursor % nodes.length;
-    nodes[cursor].classList.add("show");
-}, 10000);
