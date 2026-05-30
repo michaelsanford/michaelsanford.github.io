e32a6ab27ca6a913e6ccb7f6ad10caafed2a7f96
Michael Sanford <michaelsanford@users.noreply.github.com>
2026-05-30 19:41:48 -0400

    Init!

---
+++ README.md
@@ -0,0 +1,19 @@
+# michaelsanford.com
+
+This is my minimal landing page.
+
+# Changes from Dinesh's template
+
+Being a polyglot developer _and human_, I added multi-language support.
+
+The hero element is translated into the languages in which I feel like communicating.
+
+But rather than just show one, for the extremely rare case in which a user has more than one language support configured, it will invoke a carousel-like behaviour and rotate among only the languages the user has claimed to support. This is the case for my browser.
+
+I also hardened the security somewhat by adding a CSP, Feature Policy and an app manifest.
+
+## License
+
+This is a fork from @flexdinesh's nice project.
+
+MIT © Dinesh Pandiyan
---
+++ js/main.js
@@ -0,0 +1,20 @@
+const want = navigator.languages.map(lang => lang.split("-")[0]);
+const available = [...document.querySelectorAll("section")].map(e => e.getAttribute("lang"));
+const nodes = [...document.querySelectorAll("section")];
+
+if (want.length > 1) {
+
+    let cursor = 0;
+    setInterval(() => {
+        nodes[cursor].classList.remove("show");
+        cursor = ++cursor % nodes.length;
+        nodes[cursor].classList.add("show");
+    }, 20000);
+} else {
+    // const preferred = available.filter(o => want.includes(o));
+    [...document.querySelectorAll("section")]
+    .forEach(e => e.getAttribute("lang") === preferred[0].split("-")[0] ?
+        e.classList.add("show") :
+        e.classList.remove("show")
+    );
+}
